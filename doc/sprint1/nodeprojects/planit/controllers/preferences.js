b1e0cb75c059da2778f16496c7d6c1f8114c23df
AnsonTran <trananson@hotmail.com>
2019-10-22 09:42:26 -0400

    PLAN-24 Store user preferences and user history to user account

---
+++ doc/sprint1/nodeprojects/planit/controllers/preferences.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var router = express.Router();
+var { User } = require('../models/User');
+
+// => localhost:3000/users/addPreference/:name
+router.post('/addPreference/:name', function(req, res) {
+    // Get the user from the database
+    User.findById(req.params.name, function(err, doc) {
+        if (err){console.log('Error in finding user : ' + JSON.stringify(err, undefined, 2)); return}
+        doc.preferences.push(req.body.preferences);
+        doc.save(function(err, doc) {
+            if (!err) {res.send(doc);}
+            else { console.log('Error in updating user : ' + JSON.stringify(err, undefined, 2));}
+        })
+    })
+})
+
+// => localhost:3000/users/addHistory/:name
+router.post('/addHistory/:name', function(req, res) {
+    // Get the user from the database
+    User.findById(req.params.name, function(err, doc) {
+        if (err){console.log('Error in finding user : ' + JSON.stringify(err, undefined, 2)); return}
+        doc.history.push(req.body.history);
+        doc.save(function(err, doc) {
+            if (!err) {res.send(doc);}
+            else { console.log('Error in updating user : ' + JSON.stringify(err, undefined, 2));}
+        })
+    })
+})
+
+
+module.exports = router;
