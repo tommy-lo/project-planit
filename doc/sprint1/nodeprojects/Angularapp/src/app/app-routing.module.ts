ec886ab00608734117ddf6116e741913377060de
Joon-Hong <joon.hong1998@gmail.com>
2019-10-23 19:15:48 -0400

    PLAN-16 added signup page

--- doc/sprint1/nodeprojects/Angularapp/src/app/app-routing.module.ts
+++ doc/sprint1/nodeprojects/Angularapp/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DistanceComponent } from './distance/distance.component';
 import { TestComponent } from './test/test.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/distances', pathMatch: 'full'},
   {path: 'distances', component: DistanceComponent},
   {path: 'test', component: TestComponent},
+  {path: 'signup', component: SignupComponent},
+
   //Have least specific on the bottom otherwise you will always get page not found
   {path: "**", component: PageNotFoundComponent}
 ];
@@ -18,4 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DistanceComponent, TestComponent, PageNotFoundComponent]
+export const routingComponents = [DistanceComponent, TestComponent, PageNotFoundComponent, SignupComponent]
