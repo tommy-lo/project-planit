5ede1ee7b9f8b33b525619f633f4b28b3240f8a9
Joon-Hong <joon.hong1998@gmail.com>
2019-10-23 19:14:36 -0400

    PLAN-16 Added user service and model

---
+++ doc/sprint1/nodeprojects/Angularapp/src/app/shared/user.=model.ts
@@ -0,0 +1,7 @@
+export class User {
+    _id: string;
+    name: String;
+    password: String;
+	history: [String];
+	preferences: [String];
+}
---
+++ doc/sprint1/nodeprojects/Angularapp/src/app/shared/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user.=model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  newuser: User;
+  readonly baseURL = 'http://localhost:3000/users';
+  constructor(private http: HttpClient) { }
+
+  addUser(user: User){
+    return this.http.post(this.baseURL + "/addUser", user);
+  }
+}
